e48b90139b56ffcaf89ca35ead4705412f88abed
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-06-28 23:28:40 -0300

    init: commit inicial

--- src/script.js
+++ src/script.js
@@ -1,5 +1,26 @@
 class CatDog {
-  static baseCat = "https://api.thecatapi.com";
-  static baseDog = "https://api.thedogapi.com";
-  
+  static createImgPet(image, classImage) {
+    const imgPet = document.querySelector(`.${classImage}`);
+
+    imgPet.src = image.url;
+    imgPet.alt = image.name;
+  }
+  static getImgCat() {
+    fetch(`https://api.thecatapi.com/v1/images/search`)
+      .then((response) => response.json())
+      .then((res) => CatDog.createImgPet(res[0], "cat"));
+  }
+  static async getImgDog() {
+    await fetch(`https://api.thedogapi.com/v1/images/search`)
+      .then((response) => response.json())
+      .then((res) => CatDog.createImgPet(res[0], "dog"));
+  }
 }
+
+document.body.addEventListener("click", (event) => {
+  if (event.target.id === "cat--button") {
+    CatDog.getImgCat();
+  } else if (event.target.id === "dog--button") {
+    CatDog.getImgDog();
+  }
+});
